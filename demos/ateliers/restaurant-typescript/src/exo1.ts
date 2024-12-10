c8a0e75c0830b9bed78df03eb1c227a393e16781
Marine Lancien (Hexagone IT) <marine.lancien@hexagone-it.fr>
2024-12-10 14:48:13 +0100

    exo2 - fonctions

--- demos/ateliers/restaurant-typescript/src/exo1.ts
+++ demos/ateliers/restaurant-typescript/src/exo1.ts
@@ -1,9 +1,9 @@
-// Déclaration des types primitifs
-const nomRestaurant: string = "Chez Typescript";
-const nombreDeTables: number = 20;
-const estOuvert: boolean = true;
+let nomRestaurant: string;
+let nombreDeTables: number;
+let estOuvert: boolean;
 
-// Autres exemples possibles
-const nomProprietaire: string = "Jean Dupont";
-const anneeCreation: number = 1995;
-const proposeLivraison: boolean = false;
+
+// Déclaration des types primitifs
+nomRestaurant = "Chez Typescript";
+nombreDeTables = 20;
+estOuvert = true;
